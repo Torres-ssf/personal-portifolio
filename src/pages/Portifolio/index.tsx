926c9a178e6a99c574aaba7486c0f1953617a5f0
Torres <sergio.uft@gmail.com>
2020-07-29 14:29:50 -0300

    feat: add Header component to portifolio page

--- src/pages/Portifolio/index.tsx
+++ src/pages/Portifolio/index.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
-const Portifolio: React.FC = () => <h1>Hello World</h1>;
+import Header from '../../components/Header';
+
+import { Container } from './styles';
+
+const Portifolio: React.FC = () => {
+  const [drawerOpened, serDrawerOpened] = useState(false);
+
+  const handleDrawerOpen = useCallback(() => {
+    serDrawerOpened(oldState => !oldState);
+  }, []);
+
+  return (
+    <Container>
+      <Header drawerOpened={drawerOpened} toggleDrawer={handleDrawerOpen} />
+    </Container>
+  );
+};
 
 export default Portifolio;
