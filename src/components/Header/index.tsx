319676ce66ebd83480633899f4b6372537ed7566
Torres <sergio.uft@gmail.com>
2020-07-30 19:39:22 -0300

    reafactor: remove social links from Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {
-  Container,
-  Navbar,
-  Logo,
-  SectionList,
-  SocialList,
-  SocialIcon,
-  Drawer,
-} from './styles';
-
-import linkedinLogo from '../../assets/icons/linkedin.svg';
-import twitterLogo from '../../assets/icons/twitter.svg';
-import githubLogo from '../../assets/icons/github.svg';
-import instagramLogo from '../../assets/icons/instagram.svg';
+import { Container, Navbar, Logo, SectionList, Drawer } from './styles';
 
 interface HeaderProps {
   drawerOpened: boolean;
@@ -25,7 +12,7 @@ const Header: React.FC<HeaderProps> = ({ drawerOpened, toggleDrawer }) => {
   return (
     <Container drawerOpened={drawerOpened}>
       <Navbar>
-        <Logo>Sérgio Torres</Logo>
+        <Logo href="/">Sérgio Torres</Logo>
         <SectionList>
           <li>
             <a href="/#projects">Projects</a>
@@ -37,44 +24,7 @@ const Header: React.FC<HeaderProps> = ({ drawerOpened, toggleDrawer }) => {
             <a href="/#contact">Contact</a>
           </li>
         </SectionList>
-        <SocialList>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/torres-ssf/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <SocialIcon src={linkedinLogo} alt="linkedin" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://twitter.com/torres_ssf"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <SocialIcon src={twitterLogo} alt="twitter" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/Torres-ssf"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <SocialIcon src={githubLogo} alt="github" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/torres.ssf/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <SocialIcon src={instagramLogo} alt="instagram" />
-            </a>
-          </li>
-        </SocialList>
+
         <Drawer onClick={toggleDrawer} />
       </Navbar>
     </Container>
